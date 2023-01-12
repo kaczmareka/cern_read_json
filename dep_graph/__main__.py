513ea7ec8d5602db7b640295801b2d39b85c81a0
Agata Kaczmarek <56119853+kaczmareka@users.noreply.github.com>
2023-01-12 18:13:34 +0100

    add second version
    all working, but comments needs to be added

--- dep_graph/__main__.py
+++ dep_graph/__main__.py
@@ -12,21 +12,26 @@ def function(file_path):
     #get the keys of the dictionary
     keys=list(ordered_json_content.keys())
 
-    def print_name(values,indent):
+    def print_name(values, indent, to_print):
         for value in values:
             if value in keys:
-                print("  "*indent+f"-{value}")
+                to_print+=str("  "*indent+f"-{value}"+"\n")
                 indent+=1
-                print_name(ordered_json_content[value],indent)
+                to_print=print_name(ordered_json_content[value],indent, to_print)
                 indent-=1
             else:
-                print("  "*indent+f"-{value}")
-        
-    print_name(keys,0)
+                to_print+=str("  "*indent+f"-{value}"+"\n")
+        return to_print
+
+    to_print =""
+
+    returned=print_name(keys,0, to_print)
+    return returned
 
 def main():
     print("Hello you!") #prints to stdout by definition, according to python documentation
-    function('tmp\deps.json')
+    variable=function('tmp\deps2.json')
+    print(variable)
 
 if __name__=="__main__":
     main()
