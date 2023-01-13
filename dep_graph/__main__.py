75a5063c3d907f80ebbe75c7ac54b54a91eb9165
Agata Kaczmarek <56119853+kaczmareka@users.noreply.github.com>
2023-01-13 13:24:50 +0100

    formatted lines lengths

--- dep_graph/__main__.py
+++ dep_graph/__main__.py
@@ -4,7 +4,8 @@ from . import json_to_graph
 def main():
     """
     Main function of the dep_graph module.
-    Uses the json_to_graph function to print the dependency graph to stdout.
+    Uses the json_to_graph function to print 
+    the dependency graph to stdout.
     File path is hardcoded to 'tmp\deps.json'.
     """
     to_print=json_to_graph.json_to_graph_function('tmp\\deps.json')
